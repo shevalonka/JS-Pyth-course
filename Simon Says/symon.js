ede19d610f60967e22dbe30b192f23e2280afc62
shevalonka <89537817+shevalonka@users.noreply.github.com>
2024-05-28 00:16:42 +0300

    Update symon.js

--- Simon Says/symon.js
+++ Simon Says/symon.js
@@ -1,13 +1,27 @@
 let sequence = [];
-let humanSequence = [];
-let level = 0;
+let playerSequence = [];
+let level = 1;
 const colors = ['blue', 'yellow', 'red', 'green'];
 
 const startButton = document.getElementById('start');
-const colorButtons = document.map(color => document.getElementById('color-button'));
+const colorButtons = document.getElementsByClassName('color-button');
 const levelButton = document.getElementById('level');
+const repeatClick = document.getElementById('repeat-click');
+const levelElement = document.getElementById('level');
+
+function start() {
+    console.log('the player starts game');
+    startButton.style.display = 'none';
+    repeatClick.style.display = 'flex';
+    levelElement.textContent = 'Level ' + level;
+}
 
 startButton.addEventListener('click', start);
-colorButtons.forEach(button => {
-    button.addEventListener('click', () => handlePlayerInput(button.id));
-});
+
+
+for (let button of colorButtons) {
+    button.addEventListener('click', () => {
+        playerSequence.push(button.id);
+        console.log(playerSequence);
+    })
+}
